d722b61a971902b9d1242bdb29882c4dea23bdbf
Kyung Un Choi <kyung.choi@ingka.com>
2020-07-14 15:16:20 +0200

    add demo file

---
+++ demo.tf
@@ -0,0 +1,110 @@
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name = "static-content-bucket"
+}
+
+resource "google_redis_instance" "cache" {
+  name           = "test"
+  tier           = "STANDARD_HA"
+  memory_size_gb = 1
+  connect_mode   = "PRIVATE_SERVICE_ACCESS"
+  redis_version  = "REDIS_3_2"
+}
+
+resource "google_compute_instance" "apps" {
+  count        = 8
+  name         = "apps-${count.index + 1}"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-1804"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+locals {
+  onprem = ["192.168.1.2", "192.168.2.3"]
+}
+
+resource "google_sql_database_instance" "postgres" {
+  name             = "postgres-instance-${random_id.db_name_suffix.hex}"
+  database_version = "POSTGRES_11"
+
+  settings {
+    tier = "db-f1-micro"
+
+    ip_configuration {
+
+      dynamic "authorized_networks" {
+        for_each = google_compute_instance.apps
+        iterator = apps
+
+        content {
+          name  = apps.value.name
+          value = apps.value.network_interface.0.access_config.0.nat_ip
+        }
+      }
+
+      dynamic "authorized_networks" {
+        for_each = local.onprem
+        iterator = onprem
+
+        content {
+          name  = "onprem-${onprem.key}"
+          value = onprem.value
+        }
+      }
+    }
+  }
+}
